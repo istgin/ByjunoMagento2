b168ae69f98a5379a3e7bb66714f12d0788b3f61
UGCS-sdk <sdk@ugcs.com>
2016-11-10 12:29:27 +0200

    .

--- Byjuno/ByjunoCore/Controller/Checkout/Startpayment.php
+++ Byjuno/ByjunoCore/Controller/Checkout/Startpayment.php
@@ -3,6 +3,7 @@
  * Copyright © 2015 Pay.nl All rights reserved.
  */
 namespace Byjuno\ByjunoCore\Controller\Checkout;
+
 use Byjuno\ByjunoCore\Helper\DataHelper;
 use Byjuno\ByjunoCore\Helper\Api\ByjunoLogger;
 use Magento\Framework\App\Action\Action;
@@ -35,86 +36,147 @@ class Startpayment extends Action
         ByjunoLogger $logger
     )
     {
-       // $this->_response = $response;
-       // $this->_communicator = $communicator;
         $this->_checkoutSession = $checkoutSession;
         $this->_dataHelper = $helper;
         $this->_logger = $logger;
         parent::__construct($context);
     }
+
+    public function executeS3($order, \Magento\Sales\Model\Order\Payment $payment)
+    {
+        $request = $this->_dataHelper->CreateMagentoShopRequestPaid($order, $payment, 'transactionumber', '', '');
+        $ByjunoRequestName = "Order paid";
+        $requestType = 'b2c';
+        if ($request->getCompanyName1() != '' && $this->_dataHelper->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/businesstobusiness', \Magento\Store\Model\ScopeInterface::SCOPE_STORE) == 'enable') {
+            $ByjunoRequestName = "Order paid for Company";
+            $requestType = 'b2b';
+            $xml = $request->createRequestCompany();
+        } else {
+            $xml = $request->createRequest();
+        }
+        $mode = $this->_dataHelper->_scopeConfig->getValue('payment/cdp/currentmode', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        if ($mode == 'production') {
+            $this->_dataHelper->_communicator->setServer('live');
+        } else {
+            $this->_dataHelper->_communicator->setServer('test');
+        }
+        $response = $this->_dataHelper->_communicator->sendRequest($xml, (int)$this->_dataHelper->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/timeout', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+        $status = 0;
+        if ($response) {
+            $this->_dataHelper->_response->setRawResponse($response);
+            $this->_dataHelper->_response->processResponse();
+            $status = (int)$this->_dataHelper->_response->getCustomerRequestStatus();
+            if (intval($status) > 15) {
+                $status = 0;
+            }
+            //$helper->saveLog($quote, $request, $xml, $response, $status, $ByjunoRequestName);
+        } else {
+           // $helper->saveLog($quote, $request, $xml, "empty response", "0", $ByjunoRequestName);
+        }
+        return array($status, $requestType);
+    }
+
+    public function executeS2($order, \Magento\Sales\Model\Order\Payment $payment)
+    {
+        $request = $this->_dataHelper->CreateMagentoShopRequestOrder($order, $payment, '', '');
+
+        $ByjunoRequestName = "Order request";
+        $requestType = 'b2c';
+        if ($request->getCompanyName1() != '' && $this->_dataHelper->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/businesstobusiness', \Magento\Store\Model\ScopeInterface::SCOPE_STORE) == 'enable') {
+            $ByjunoRequestName = "Order request for Company";
+            $requestType = 'b2b';
+            $xml = $request->createRequestCompany();
+        } else {
+            $xml = $request->createRequest();
+        }
+        $mode = $this->_dataHelper->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/currentmode', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        if ($mode == 'production') {
+            $this->_dataHelper->_communicator->setServer('live');
+        } else {
+            $this->_dataHelper->_communicator->setServer('test');
+        }
+        $response = $this->_dataHelper->_communicator->sendRequest($xml, (int)$this->_dataHelper->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/timeout', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+        $status = 0;
+        if ($response) {
+            $this->_dataHelper->_response->setRawResponse($response);
+            $this->_dataHelper->_response->processResponse();
+            $status = (int)$this->_dataHelper->_response->getCustomerRequestStatus();
+            $this->_checkoutSession->setByjunoTransaction($this->_dataHelper->_response->getTransactionNumber());
+            //$this->_dataHelper->saveLog($quote, $request, $xml, $response, $status, $ByjunoRequestName);
+            if (intval($status) > 15) {
+                $status = 0;
+            }
+            $trxId = $this->_dataHelper->_response->getResponseId();
+        } else {
+            //$this->getHelper()->saveLog($quote, $request, $xml, "empty response", "0", $ByjunoRequestName);
+            $trxId = "empty";
+        }
+        $payment->setTransactionId($trxId);
+        $payment->setParentTransactionId($payment->getTransactionId());
+
+        $transaction = $payment->addTransaction(\Magento\Sales\Model\Order\Payment\Transaction::TYPE_AUTH, null, true);
+        if ($status == 2) {
+            $transaction->setIsClosed(false);
+        } else {
+            $transaction->setIsClosed(true);
+        }
+        $transaction->save();
+        $payment->save();
+
+        $this->_checkoutSession->setIntrumStatus("intrum_status", $status);
+        $this->_checkoutSession->setIntrumRequestType("intrum_request_type", $requestType);
+        $this->_checkoutSession->setIntrumOrder("intrum_order", $order->getId());
+        //$this->_dataHelper->_byjunoOrderSender->send($order);
+        //exit();
+        return array($status, $requestType, $this->_dataHelper->_response);
+    }
+
     public function execute()
     {
+        $order = $this->_checkoutSession->getLastRealOrder();
+        /* @var $payment \Magento\Sales\Model\Order\Payment */
+        $payment = $order->getPayment();
 
+        $resultRedirect = $this->resultRedirectFactory->create();
         try {
-            $order = $this->_checkoutSession->getLastRealOrder();
-            /* @var $payment \Magento\Sales\Model\Order\Payment */
-            $payment = $order->getPayment();
-            $request = $this->_dataHelper->CreateMagentoShopRequestOrder($order, $payment, '', '');
+            list($statusS2, $requestTypeS2, $responseS2) = $this->executeS2($order, $payment);
+            if ($statusS2 == 2) {
+                list($statusS3, $requestTypeS3) = $this->executeS3($order, $payment);
 
-            $ByjunoRequestName = "Order request";
-            $requestType = 'b2c';
-            if ($request->getCompanyName1() != '' && $this->_dataHelper->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/businesstobusiness', \Magento\Store\Model\ScopeInterface::SCOPE_STORE) == 'enable') {
-                $ByjunoRequestName = "Order request for Company";
-                $requestType = 'b2b';
-                $xml = $request->createRequestCompany();
-            } else {
-                $xml = $request->createRequest();
-            }
-            $mode = $this->_dataHelper->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/currentmode', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-            if ($mode == 'production') {
-                $this->_dataHelper->_communicator->setServer('live');
-            } else {
-                $this->_dataHelper->_communicator->setServer('test');
-            }
-            $response = $this->_dataHelper->_communicator->sendRequest($xml, (int)$this->_dataHelper->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/timeout', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
-            $status = 0;
-            if ($response) {
-                $this->_dataHelper->_response->setRawResponse($response);
-                $this->_dataHelper->_response->processResponse();
-                $status = (int)$this->_dataHelper->_response->getCustomerRequestStatus();
-                $this->_checkoutSession->setByjunoTransaction($this->_dataHelper->_response->getTransactionNumber());
-                //$this->_dataHelper->saveLog($quote, $request, $xml, $response, $status, $ByjunoRequestName);
-                if (intval($status) > 15) {
-                    $status = 0;
+                if ($statusS3 == 2) {
+                    $payment->setAdditionalInformation("s3_ok", 'true')->save();
+                    $order->setState(\Magento\Sales\Model\Order::STATE_PROCESSING);
+                    if (!empty($statusToPayment) && !empty($ByjunoResponseSession)) {
+                        $this->_dataHelper->saveStatusToOrder($order, $responseS2);
+                    }
+                    $order->save();
+                    try {
+                        $this->_dataHelper->_byjunoOrderSender->send($order);
+                    } catch (\Exception $e) {
+                        $this->_dataHelper->_loggerPsr->critical($e);
+                    }
+                    $resultRedirect->setPath('checkout/onepage/success');
+                } else {
+                    $error = $this->_dataHelper->getByjunoErrorMessage($statusS3, $requestTypeS3). "(S3)";
+                    $order->registerCancellation($error)->save();
+                    $this->_checkoutSession->restoreQuote();
+                    $this->messageManager->addExceptionMessage(new \Exception($statusS3), $error);
+                    $resultRedirect->setPath('checkout/cart');
                 }
-                $trxId = $this->_dataHelper->_response->getResponseId();
-            } else {
-                //$this->getHelper()->saveLog($quote, $request, $xml, "empty response", "0", $ByjunoRequestName);
-                $trxId = "empty";
-            }
-            $payment->setTransactionId($trxId);
-            $payment->setParentTransactionId($payment->getTransactionId());
-
-            $transaction = $payment->addTransaction(\Magento\Sales\Model\Order\Payment\Transaction::TYPE_AUTH, null, true);
-            if ($status == 2) {
-                $transaction->setIsClosed(false);
-            } else {
-                $transaction->setIsClosed(true);
-            }
-            $transaction->save();
-            $payment->save();
-
-            $this->_checkoutSession->setIntrumStatus("intrum_status", $status);
-            $this->_checkoutSession->setIntrumRequestType("intrum_request_type", $requestType);
-            $this->_checkoutSession->setIntrumOrder("intrum_order", $order->getId());
-            $resultRedirect = $this->resultRedirectFactory->create();
-            //$this->_dataHelper->_byjunoOrderSender->send($order);
-            //exit();
-            if ($status == 2) {
-                $resultRedirect->setPath('checkout/onepage/success');
-            } else if ($status == 0) {
-                $error = $this->_dataHelper->getByjunoErrorMessage($status, $requestType);
+            } else if ($statusS2 == 0) {
+                $error = $this->_dataHelper->getByjunoErrorMessage($statusS2, $requestTypeS2);
                 $order->registerCancellation($error)->save();
                 $this->_checkoutSession->restoreQuote();
-                $this->messageManager->addExceptionMessage(new \Exception($status), $error);
+                $this->messageManager->addExceptionMessage(new \Exception($statusS2), $error);
                 $resultRedirect->setPath('checkout/cart');
             } else {
-                $error = $this->_dataHelper->getByjunoErrorMessage($status, $requestType);
+                $error = $this->_dataHelper->getByjunoErrorMessage($statusS2, $requestTypeS2);
                 $order->registerCancellation($error)->save();
                 $this->_checkoutSession->restoreQuote();
-                $this->messageManager->addExceptionMessage(new \Exception($status), $error);
+                $this->messageManager->addExceptionMessage(new \Exception($statusS2), $error);
                 $resultRedirect->setPath('checkout/cart');
             }
+
         } catch (\Exception $e) {
             $order = $this->_checkoutSession->getLastRealOrder();
             $error = __("Unexpected error");
@@ -125,6 +187,5 @@ class Startpayment extends Action
             $resultRedirect->setPath('checkout/cart');
         }
         return $resultRedirect;
-
     }
 }
--- Byjuno/ByjunoCore/Helper/DataHelper.php
+++ Byjuno/ByjunoCore/Helper/DataHelper.php
@@ -14,6 +14,11 @@ class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper {
     protected $_resolver;
     public $_byjunoOrderSender;
     /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    public $_loggerPsr;
+
+    /**
      * @var \Byjuno\ByjunoCore\Helper\Api\ByjunoCommunicator
      */
     public $_communicator;
@@ -21,6 +26,29 @@ class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper {
      * @var \Byjuno\ByjunoCore\Helper\Api\ByjunoResponse
      */
     public $_response;
+
+    public function valueToStatus($val) {
+        $status[0] = 'Fail to connect (status Error)';
+        $status[1] = 'There are serious negative indicators (status 1)';
+        $status[2] = 'All payment methods allowed (status 2)';
+        $status[3] = 'Manual post-processing (currently not yet in use) (status 3)';
+        $status[4] = 'Postal address is incorrect (status 4)';
+        $status[5] = 'Enquiry exceeds the credit limit (the credit limit is specified in the cooperation agreement) (status 5)';
+        $status[6] = 'Customer specifications not met (optional) (status 6)';
+        $status[7] = 'Enquiry exceeds the net credit limit (enquiry amount plus open items exceeds credit limit) (status 7)';
+        $status[8] = 'Person queried is not of creditworthy age (status 8)';
+        $status[9] = 'Delivery address does not match invoice address (for payment guarantee only) (status 9)';
+        $status[10] = 'Household cannot be identified at this address (status 10)';
+        $status[11] = 'Country is not supported (status 11)';
+        $status[12] = 'Party queried is not a natural person (status 12)';
+        $status[13] = 'System is in maintenance mode (status 13)';
+        $status[14] = 'Address with high fraud risk (status 14)';
+        $status[15] = 'Allowance is too low (status 15)';
+        if (isset($status[$val])) {
+            return $status[$val];
+        }
+        return $status[0];
+    }
 
     public function getClientIp() {
         $ipaddress = '';
@@ -90,6 +118,16 @@ class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper {
         return $message;
     }
 
+    public function saveStatusToOrder(\Magento\Sales\Model\Order $order, \Byjuno\ByjunoCore\Helper\Api\ByjunoResponse $byjunoS2Response) {
+        $order->addStatusHistoryComment('<b>Byjuno status: '.$this->valueToStatus($byjunoS2Response->getCustomerRequestStatus()).'</b>
+        <br/>Credit rating: '.$this->_response->getCustomerCreditRating().'
+        <br/>Credit rating level: '.$this->_response->getCustomerCreditRatingLevel().'<br/>Status code: '. $byjunoS2Response->getCustomerRequestStatus().'</b>');
+        $order->setByjunoStatus($byjunoS2Response->getCustomerRequestStatus());
+        $order->setByjunoCreditRating($byjunoS2Response->getCustomerCreditRating());
+        $order->setByjunoCreditLevel($byjunoS2Response->getCustomerCreditRatingLevel());
+        $order->save();
+    }
+
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
         \Magento\Backend\Model\Menu\Filter\IteratorFactory $iteratorFactory,
@@ -102,7 +140,8 @@ class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper {
         \Magento\Framework\Locale\Resolver $resolver,
         \Byjuno\ByjunoCore\Helper\Api\ByjunoCommunicator $communicator,
         \Byjuno\ByjunoCore\Helper\Api\ByjunoResponse $response,
-        \Byjuno\ByjunoCore\Helper\ByjunoOrderSender $byjunoOrderSender
+        \Byjuno\ByjunoCore\Helper\ByjunoOrderSender $byjunoOrderSender,
+        \Psr\Log\LoggerInterface $loggerPsr
     )
     {
 
@@ -265,6 +304,7 @@ class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper {
             $request->setExtraInfo($extraInfo);
 
             if ($order->getShippingAddress()->getCompany() != '' && $this->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/businesstobusiness', \Magento\Store\Model\ScopeInterface::SCOPE_STORE) == 'enable') {
+
                 $extraInfo["Name"] = 'DELIVERY_COMPANYNAME';
                 $extraInfo["Value"] = $order->getShippingAddress()->getCompany();
                 $request->setExtraInfo($extraInfo);
@@ -313,6 +353,199 @@ class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper {
         $extraInfo["Value"] = 'Byjuno Magento 2.1 module 1.0.0';
         $request->setExtraInfo($extraInfo);
         return $request;
+    }
+
+    function CreateMagentoShopRequestPaid(\Magento\Sales\Model\Order $order,
+                                          \Magento\Sales\Model\Order\Payment $paymentmethod,
+                                          $gender_custom, $dob_custom, $transaction) {
+
+        $request = new \Byjuno\ByjunoCore\Helper\Api\ByjunoRequest();
+        $request->setClientId($this->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/clientid', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+        $request->setUserID($this->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/userid', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+        $request->setPassword($this->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/password', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+        $request->setVersion("1.00");
+        try {
+            $request->setRequestEmail($this->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/mail', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+        } catch (\Exception $e) {
+
+        }
+        $b = $order->getCustomerDob();
+        if (!empty($b)) {
+            try {
+                $dobObject = new \DateTime($b);
+                if ($dobObject != null) {
+                    $request->setDateOfBirth($dobObject->format('Y-m-d'));
+                }
+            } catch (\Exception $e) {
+
+            }
+        }
+
+        if (!empty($dob_custom)) {
+            try {
+                $dobObject = new \DateTime($dob_custom);
+                if ($dobObject != null) {
+                    $request->setDateOfBirth($dobObject->format('Y-m-d'));
+                }
+            } catch (\Exception $e) {
+
+            }
+        }
+
+
+        $g = $order->getCustomerGender();
+        if (!empty($g)) {
+            if ($g == '1') {
+                $request->setGender('1');
+            } else if ($g == '2') {
+                $request->setGender('2');
+            } else {
+                $request->setGender('0');
+            }
+        } else {
+            if (strtolower($order->getCustomerPrefix()) == 'herr') {
+                $request->setGender('1');
+            } else if (strtolower($order->getCustomerPrefix()) == 'frau') {
+                $request->setGender('2');
+            } else {
+                $request->setGender('0');
+            }
+        }
+
+        if (!empty($gender_custom)) {
+            if ($gender_custom == '1') {
+                $request->setGender('1');
+            } else if ($gender_custom == '2') {
+                $request->setGender('2');
+            }
+        }
+        $billingStreet = $order->getBillingAddress()->getStreet();
+        $billingStreet = implode("", $billingStreet);
+        $requestId = uniqid((String)$order->getBillingAddress()->getEntityId()."_");
+        $request->setRequestId($requestId);
+        $reference = $order->getCustomerId();
+        if (empty($reference)) {
+            $request->setCustomerReference("guest_".$order->getId());
+        } else {
+            $request->setCustomerReference($order->getCustomerId());
+        }
+        $request->setFirstName((String)$order->getBillingAddress()->getFirstname());
+        $request->setLastName((String)$order->getBillingAddress()->getLastname());
+        //quote.billingAddress().street[0] + ", " + quote.billingAddress().city + ", " + quote.billingAddress().postcode
+        $request->setFirstLine(trim((String)$billingStreet));
+        $request->setCountryCode(strtoupper($order->getBillingAddress()->getCountryId()));
+        $request->setPostCode((String)$order->getBillingAddress()->getPostcode());
+        $request->setTown((String)$order->getBillingAddress()->getCity());
+        $request->setFax((String)trim($order->getBillingAddress()->getFax(), '-'));
+        $request->setLanguage((String)substr($this->_resolver->getLocale(), 0, 2));
+
+        if ($order->getBillingAddress()->getCompany()) {
+            $request->setCompanyName1($order->getBillingAddress()->getCompany());
+        }
+
+        $request->setTelephonePrivate((String)trim($order->getBillingAddress()->getTelephone(), '-'));
+        $request->setEmail((String)$order->getBillingAddress()->getEmail());
+
+        $extraInfo["Name"] = 'TRANSACTIONNUMBER';
+        $extraInfo["Value"] = $transaction;
+        $request->setExtraInfo($extraInfo);
+
+        $extraInfo["Name"] = 'ORDERCLOSED';
+        $extraInfo["Value"] = 'YES';
+        $request->setExtraInfo($extraInfo);
+
+        $extraInfo["Name"] = 'ORDERAMOUNT';
+        $extraInfo["Value"] = number_format($order->getGrandTotal(), 2, '.', '');
+        $request->setExtraInfo($extraInfo);
+
+        $extraInfo["Name"] = 'ORDERCURRENCY';
+        $extraInfo["Value"] = $order->getBaseCurrencyCode();
+        $request->setExtraInfo($extraInfo);
+
+        $extraInfo["Name"] = 'IP';
+        $extraInfo["Value"] = $this->getClientIp();
+        $request->setExtraInfo($extraInfo);
+
+        if ($paymentmethod->getAdditionalInformation('invoice_send') == 'postal') {
+            $extraInfo["Name"] = 'PAPER_INVOICE';
+            $extraInfo["Value"] = 'YES';
+            $request->setExtraInfo($extraInfo);
+        }
+
+        if ($order->canShip()) {
+
+            $shippingStreet = $order->getShippingAddress()->getStreet();
+            $shippingStreet = implode("", $shippingStreet);
+
+            $extraInfo["Name"] = 'DELIVERY_FIRSTLINE';
+            $extraInfo["Value"] = trim($shippingStreet);
+            $request->setExtraInfo($extraInfo);
+
+            $extraInfo["Name"] = 'DELIVERY_HOUSENUMBER';
+            $extraInfo["Value"] = '';
+            $request->setExtraInfo($extraInfo);
+
+            $extraInfo["Name"] = 'DELIVERY_COUNTRYCODE';
+            $extraInfo["Value"] = strtoupper($order->getShippingAddress()->getCountryId());
+            $request->setExtraInfo($extraInfo);
+
+            $extraInfo["Name"] = 'DELIVERY_POSTCODE';
+            $extraInfo["Value"] = $order->getShippingAddress()->getPostcode();
+            $request->setExtraInfo($extraInfo);
+
+            $extraInfo["Name"] = 'DELIVERY_TOWN';
+            $extraInfo["Value"] = $order->getShippingAddress()->getCity();
+            $request->setExtraInfo($extraInfo);
+
+            if ($order->getShippingAddress()->getCompany() != '' && $this->_scopeConfig->getValue('byjunocheckoutsettings/byjuno_setup/businesstobusiness', \Magento\Store\Model\ScopeInterface::SCOPE_STORE) == 'enable') {
+
+                $extraInfo["Name"] = 'DELIVERY_COMPANYNAME';
+                $extraInfo["Value"] = $order->getShippingAddress()->getCompany();
+                $request->setExtraInfo($extraInfo);
+
+                $extraInfo["Name"] = 'DELIVERY_FIRSTNAME';
+                $extraInfo["Value"] = '';
+                $request->setExtraInfo($extraInfo);
+
+                $extraInfo["Name"] = 'DELIVERY_LASTNAME';
+                $extraInfo["Value"] = $order->getShippingAddress()->getCompany();
+                $request->setExtraInfo($extraInfo);
+
+            } else {
+
+                $extraInfo["Name"] = 'DELIVERY_FIRSTNAME';
+                $extraInfo["Value"] = $order->getShippingAddress()->getFirstname();
+                $request->setExtraInfo($extraInfo);
+
+                $extraInfo["Name"] = 'DELIVERY_LASTNAME';
+                $extraInfo["Value"] = $order->getShippingAddress()->getLastname();
+                $request->setExtraInfo($extraInfo);
+
+            }
+        }
+
+        $extraInfo["Name"] = 'ORDERID';
+        $extraInfo["Value"] = $order->getIncrementId();
+        $request->setExtraInfo($extraInfo);
+
+        $extraInfo["Name"] = 'PAYMENTMETHOD';
+        $extraInfo["Value"] = $this->mapMethod($paymentmethod->getMethod());
+        $request->setExtraInfo($extraInfo);
+
+        $extraInfo["Name"] = 'REPAYMENTTYPE';
+        $extraInfo["Value"] = $this->mapRepayment($paymentmethod->getAdditionalInformation('payment_plan'));
+        $request->setExtraInfo($extraInfo);
+
+        $extraInfo["Name"] = 'RISKOWNER';
+        $extraInfo["Value"] = 'IJ';
+        $request->setExtraInfo($extraInfo);
+
+        $extraInfo["Name"] = 'CONNECTIVTY_MODULE';
+        $extraInfo["Value"] = 'Byjuno Magento 2.1 module 1.0.0';
+        $request->setExtraInfo($extraInfo);
+
+        return $request;
+
     }
 
 /*
@@ -405,13 +638,7 @@ class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper {
     }
 
 
-    public function saveStatusToOrder($order, $byjuno_status, Byjuno_Cdp_Helper_Api_Classes_ByjunoResponse $ByjunoResponse) {
-        $order->addStatusHistoryComment('<b>Byjuno status: '.$this->valueToStatus($byjuno_status).'</b><br/>Credit rating: '.$ByjunoResponse->getCustomerCreditRating().'<br/>Credit rating level: '.$ByjunoResponse->getCustomerCreditRatingLevel().'<br/>Status code: '. $byjuno_status.'</b>');
-        $order->setByjunoStatus($byjuno_status);
-        $order->setByjunoCreditRating($ByjunoResponse->getCustomerCreditRating());
-        $order->setByjunoCreditLevel($ByjunoResponse->getCustomerCreditRatingLevel());
-        $order->save();
-    }
+
 
     function CreateMagentoShopRequestPaid(Mage_Sales_Model_Order $order, $paymentmethod, $repayment, $transaction, $invoiceDelivery, $gender_custom, $dob_custom) {
 
