7e29844202c34c034e441b1c283e8bebdaadb05b
Igor Sutugin <igor.sutugin@ugcs.com>
2016-12-08 21:38:37 +0200

    .

---
+++ Byjuno/ByjunoCore/Block/Threatmetrix.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Byjuno\ByjunoCore\Block;
+
+
+class Threatmetrix extends \Magento\Framework\View\Element\Template
+{
+
+    protected function _toHtml()
+    {
+        return parent::_toHtml();
+    }
+}
